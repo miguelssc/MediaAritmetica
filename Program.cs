20ac4fb2f280c22cec239212ab1cb303e05c2918
miguel <miguelpositivo02@gmail.com>
2023-05-19 11:36:26 -0300

    exercicio resolvido

---
+++ Program.cs
@@ -0,0 +1,9 @@
+﻿Console.WriteLine("digite a primeira nota");
+decimal nota1 = Convert.ToDecimal(Console.ReadLine());
+Console.WriteLine("digite a segunda nota");
+decimal nota2 = Convert.ToDecimal(Console.ReadLine());
+Console.WriteLine("digite a terceira nota");
+decimal nota3 = Convert.ToDecimal(Console.ReadLine());
+decimal notafinal =(nota1 + nota2 + nota3);
+decimal mediafinal =(notafinal / 3);
+Console.WriteLine(mediafinal);
